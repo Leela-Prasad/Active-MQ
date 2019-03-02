e88127a877dbd0ac4d64dbdcab35ee1eb51c3fb8
Leela <leela.jagu@gmail.com>
2019-03-02 16:03:59 +0530

    Browsing Messages

--- message-receive/src/main/java/com/springboot/activemq/MessageListener.java
+++ message-receive/src/main/java/com/springboot/activemq/MessageListener.java
@@ -3,7 +3,7 @@ package com.springboot.activemq;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MessageListener {
 
 	@JmsListener(destination="sampleQueue")
--- message-receive/src/main/java/com/springboot/activemq/MessageReceiveApplication.java
+++ message-receive/src/main/java/com/springboot/activemq/MessageReceiveApplication.java
@@ -1,8 +1,16 @@
 package com.springboot.activemq;
 
+import java.util.Enumeration;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.QueueBrowser;
+import javax.jms.Session;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.BrowserCallback;
 import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
@@ -14,6 +22,22 @@ public class MessageReceiveApplication {
 		System.out.println(jmsTemplate.getReceiveTimeout());
 		jmsTemplate.setReceiveTimeout(1000);
 		System.out.println(jmsTemplate.receiveAndConvert("sampleQueue"));*/
+		
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		int messageCount = jmsTemplate.browse("positionQueue", new BrowserCallback<Integer>() {
+			@Override
+			public Integer doInJms(Session session, QueueBrowser browser) throws JMSException {
+				Enumeration<Message> messages = browser.getEnumeration();
+				int count=0;
+				while(messages.hasMoreElements()) {
+					System.out.println(messages.nextElement());
+					++count;
+				}
+				return count;
+			}
+			
+		});
+		System.out.println("Message Count :" + messageCount);
 	}
 
 }
